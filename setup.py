318182db562aa0934fd8c475098c4c9c5c117d35
Vadim Syrov <steamruler@mail.ru>
2023-12-10 17:23:18 +0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='benchmark-libs-lab',
+    version='0.0.1',
+    description='Lab on benchmarking of different data access libraries',
+    author='Vadim Syrov',
+    author_email='vasyrov@edu.hse.ru',
+    packages=find_packages(),
+    install_requires=[],
+)
